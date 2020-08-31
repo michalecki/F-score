6b242021af57928dc5d5764c16a4050a22549f15
michalecki <michal.matusewicz@gmail.com>
2020-08-31 14:13:29 +0300

    code cleaning

--- f-score.py
+++ f-score.py
@@ -6,8 +6,49 @@ The script loops through the pdf files in the folder and
 calculates F-score for each company.
 '''
 
-import os  # to acces file list in the folder
+import os  # to access file list in the folder
 import tabula  # to read in tables from pdf
+
+
+def check_tables(summary, income_statement, balance_sheet, cash_flow, year, file):
+    '''
+    checks if retrieving correct values
+    :param summary:
+    :param income_statement:
+    :param balance_sheet:
+    :param cash_flow:
+    :return:
+    '''
+    # table checks if retrieving correct data
+    try:
+        if summary.iat[0, 3] != str(year):
+            print(file, 'Check Summary table and years')
+        if cash_flow.iat[0, 8] != str(year):
+            print(file, 'Check Cash Flow table and years')
+        if balance_sheet.iat[0, 8] != str(year):
+            print(file, 'Check Balance Sheet table and years')
+        if income_statement.iat[0, 8] != str(year):
+            print(file, 'Check Income Statement table and years')
+        if income_statement.iat[18, 0][0:10] != "Net profit":
+            print(file, "Check net profit")
+        if balance_sheet.iat[20, 0][0:9] != 'Total ass':
+            print(file, 'Check total assets')
+        if cash_flow.iat[11, 0][0:9] != 'Cash flow':
+            print(file, 'Check cfo')
+        if income_statement.iat[5, 0][0:6] != 'EBITDA':
+            print(file, 'Check EBITDA')
+        if income_statement.iat[1, 0][0:13] != 'Total revenue':
+            print(file, 'Check Total revenue')
+        if balance_sheet.iat[27, 0] != 'Long term interest bearing debt':
+            print(file, 'check long-time debt')
+        if balance_sheet.iat[40, 0] != 'Short term interest bearing debt':
+            print(file, 'Check short-time debt')
+        if balance_sheet.iat[41, 0] != 'Total current liabilities':
+            print(file, 'Check liabilities')
+        if cash_flow.iat[19, 0][0:13] != 'Equity issues':
+            print(file, 'Check equity issues')
+    except IndexError:
+        print(file, 'Wrong indexing')
 
 
 def calculate_f_score(file, year=2019):
@@ -35,38 +76,15 @@ def calculate_f_score(file, year=2019):
     # title = PdfFileReader(file).getDocumentInfo().title
 
     # assigns names to the tables for clarity
-    summary = data[0]
-    income_statement = data[1]
-    balance_sheet = data[2]
-    cash_flow = data[3]
+    try:
+        summary = data[0]
+        income_statement = data[1]
+        balance_sheet = data[2]
+        cash_flow = data[3]
+    except IndexError:
+        return file, 'wrong tables input reading'
 
-    # table checks if retrieving correct data
-    if summary.iat[0, 3] != str(year):
-        print(file, 'Check Summary table and years')
-    if cash_flow.iat[0, 8] != str(year):
-        print(file, 'Check Cash Flow table and years')
-    if balance_sheet.iat[0, 8] != str(year):
-        print(file, 'Check Balance Sheet table and years')
-    if income_statement.iat[0, 8] != str(year):
-        print(file, 'Check Income Statement table and years')
-    if income_statement.iat[18, 0][0:10] != "Net profit":
-        print(file, "Check net profit")
-    if balance_sheet.iat[20, 0][0:9] != 'Total ass':
-        print(file, 'Check total assets')
-    if cash_flow.iat[11, 0][0:9] != 'Cash flow':
-        print(file, 'Check cfo')
-    if income_statement.iat[5, 0][0:6] != 'EBITDA':
-        print(file, 'Check EBITDA')
-    if income_statement.iat[1, 0][0:13] != 'Total revenue':
-        print(file, 'Check Total revenue')
-    if balance_sheet.iat[27, 0] != 'Long term interest bearing debt':
-        print(file, 'check long-time debt')
-    if balance_sheet.iat[40, 0] != 'Short term interest bearing debt':
-        print(file, 'Check short-time debt')
-    if balance_sheet.iat[41, 0] != 'Total current liabilities':
-        print(file, 'Check liabilities')
-    if cash_flow.iat[19, 0][0:13] != 'Equity issues':
-        print(file, 'Check equity issues')
+    check_tables(summary, income_statement, balance_sheet, cash_flow, year, file)
 
     try:
         p_b = summary.iat[11, y - 5]
@@ -77,14 +95,24 @@ def calculate_f_score(file, year=2019):
     f_score = 0
 
     # net income/total assets
-    roa = int(income_statement.iat[18, y].replace(',', '')) / int(
-        balance_sheet.iat[20, y].replace(',', ''))
+    try:
+        roa = int(income_statement.iat[18, y].replace(',', '')) / int(
+            balance_sheet.iat[20, y].replace(',', ''))
+    except IndexError:
+        return file, 'roa index error'
+    except ValueError:
+        return file, 'roa value error'
+
     if roa > 0:
         f_score += 1
 
     # roa difference of last two years
-    d_roa = roa - int(income_statement.iat[18, y - 1].replace(',', '')) / int(
-        balance_sheet.iat[20, y - 1].replace(',', ''))
+    try:
+        d_roa = roa - int(income_statement.iat[18, y - 1].replace(',', '')) / int(
+            balance_sheet.iat[20, y - 1].replace(',', ''))
+    except:
+        return file, 'd_roa error'
+
     if d_roa > 0:
         f_score += 1
 
@@ -99,10 +127,14 @@ def calculate_f_score(file, year=2019):
         f_score += 1
 
     # EBITDA/total revenue difference of last two years
-    d_margin = int(income_statement.iat[5, y].replace(',', '')) / int(
-        income_statement.iat[1, y].replace(',', '')) \
-               - int(income_statement.iat[5, y - 1].replace(',', '')) / int(
-        income_statement.iat[1, y - 1].replace(',', ''))
+    try:
+        d_margin = int(income_statement.iat[5, y].replace(',', '')) / int(
+            income_statement.iat[1, y].replace(',', '')) \
+                   - int(income_statement.iat[5, y - 1].replace(',', '')) / int(
+            income_statement.iat[1, y - 1].replace(',', ''))
+    except:
+        d_margin = 0
+        print(file, 'd_margin error')
     if d_margin > 0:
         f_score += 1
 
@@ -155,7 +187,7 @@ def folder_loop(directory):
     Helper function to loop through the files in a folder and
     calculate parameters for the files that don't have
     the calculation done yet.
-    :param dir: str, directory where the files are
+    :param directory: str, directory where the files are
     :return: text file with data: file name, output of f_score
     '''
     # makes a list of the files in the directory
@@ -168,6 +200,7 @@ def folder_loop(directory):
             titles.append(file.split('.')[0])
 
     # makes the parameter file or opens it for reading and appending
+    # modify the file name if calculating for different year than 2019
     try:
         fh = open(f"{directory}/parameters.txt", "r+")
     except FileNotFoundError:
@@ -186,6 +219,7 @@ def folder_loop(directory):
     # calculates parameter and adds to the file if the filename in not in the file yet
     for title in titles:
         if title not in calculated:
+            # add here the year parameter if want to calculate earlier than 2019
             fh.write(f"{title} {calculate_f_score(directory + '/' + title + '.pdf')}\n")
     fh.close()
 
@@ -193,4 +227,4 @@ def folder_loop(directory):
 # example use
 # will read files in the ./samples folder
 # will write the "parameters.txt" file into the ./samples directory
-folder_loop('samples')
+folder_loop('/samples')
